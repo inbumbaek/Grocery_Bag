70f34e893d000f0d623073e471f46eef4bf502b4
inbumbaek <inbumbaek@gmail.com>
2023-05-09 09:32:05 -0700

    client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,25 @@
+import './App.css';
+import { useState } from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Login from './components/Login';
+import Register from './components/Register';
 
+function App() {
+  const [grocery, setGrocery] = useState({
+    groceryItem:'',
+    quantity: 0
+  })
+  const [errors, setErrors] = useState({})
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
